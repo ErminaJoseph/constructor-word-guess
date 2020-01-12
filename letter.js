f9133d5452d55b7dc674fbbf351ae82d8d2f648f
Ermina Joseph <ermina.joseph@gmail.com>
2020-01-12 18:16:29 -0500

    preliminary coding

---
+++ letter.js
@@ -0,0 +1,10 @@
+var inquirer = require("inquirer");
+var Word = require("./word.js");
+
+var Letter = function() {
+    this.underlyingCharacter = "";
+    this.guessedLetter = false;
+    this.correctLetter = function() {
+
+    }
+}
